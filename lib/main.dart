05588486019e2e51b79021eebd978a810b275e0a
Oreoluwa <Odetundeoreoluwadavid@gmail.com>
2023-08-23 15:50:13 +0100

    Added both Login Page and SignUp Page with each authentication using Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:instagram_clone/core/palette.dart';
+import 'package:instagram_clone/features/signup_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Placeholder(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 725),
+      minTextAdapt: true,
+      useInheritedMediaQuery: true,
+      builder: (BuildContext context, child) =>
+      MaterialApp(
+          title: 'Davedevs Instagram Clone App',
+          theme: ThemeData.dark(useMaterial3: true).copyWith(
+            scaffoldBackgroundColor: Palette.bgColor,
+            appBarTheme: AppBarTheme(backgroundColor: Palette.bgColor)
+          ),
+          home: const SignUpPage(),
+        )
     );
   }
 }
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,8 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   dynamic_color
-  smart_auth
-  url_launcher_linux
+  file_selector_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
